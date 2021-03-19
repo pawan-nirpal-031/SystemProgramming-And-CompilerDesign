b9d80d8b3fe18aeebafda86b4610dadd138cf796
Pawan Nirpal <pawannirpal@gmail.com>
2021-03-19 22:32:16 +0530

    Error : PassOne fun returing null

--- MacroProcesser/TwoPassMacroProcesser.java
+++ MacroProcesser/TwoPassMacroProcesser.java
@@ -1,13 +1,24 @@
 import java.util.*;
 import java.io.*;
 
-
+class StringIntPair{
+    String key;
+    int value;
+    StringIntPair(){
+        key = "";
+        value = -1;
+    }
+    StringIntPair(String k,int v){
+        key = k;
+        value = v;
+    }
+}
 public class TwoPassMacroProcesser {
     static BufferedReader buffer_read = null;
     static FileWriter file_write = null;
     static BufferedWriter buff_write = null;
     static FileReader file_read = null;
-    Hashtable<String,Integer>MacroNametable; // MacroNametable(Macro Name, MDT index)
+    StringIntPair[] MacroNametable; // MacroNametable(Macro Name, MDT index)
     String[] MacroDefinationTable;// MacroDefinationTable(token)
     String[] ArgumentList;
     int mdtc =0;
@@ -16,17 +27,39 @@ public class TwoPassMacroProcesser {
 
     public String PassOneMacroProcesser() throws Exception{
         String CurrSourceLine="";
+        boolean macro_dfn = false;
         while((CurrSourceLine=buffer_read.readLine())!=null){
             String token[] = CurrSourceLine.split("[ \t]+");
-            
+            if(token[0].equals("MACRO")) macro_dfn= true;
+            if(!macro_dfn){
+                for(String t : token) file_write.write(t+' ');
+                file_write.write('\n');
+            }else{
+                if(mdtc==databasesize-1) return "Error : Macro Defination Table size exceded";
+                if(token.length>=2){
+                    boolean is_macro_name = true;
+                    for(int i =0;i<token.length;i++){
+                        if(i!=1 && (token[i].charAt(0)!='&')) is_macro_name = false;
+                    }
+                    if(is_macro_name){
+                        MacroNametable[mntc].key = token[1];
+                        MacroNametable[mntc].value = mdtc;
+                    }
+                }
+                MacroDefinationTable[mdtc] = CurrSourceLine;
+                mdtc+=1;
+                if(token[0].equals("MEND")) macro_dfn = false;
+            }
         }
-        return "Pass one successful";
+        for(String t : MacroDefinationTable) System.out.println(t);
+        return "Pass one ok";
     }
 
 
     TwoPassMacroProcesser(){
        MacroDefinationTable = new String[databasesize];
        ArgumentList = new String[databasesize];
+       MacroNametable = new StringIntPair[databasesize];
        for(int i =0;i<databasesize;i++) MacroDefinationTable[i] = ArgumentList[i] = "";
        mdtc = mntc = 0;
     }
@@ -41,8 +74,10 @@ public class TwoPassMacroProcesser {
             file_write = new FileWriter(Target_file);
             buff_write = new BufferedWriter(file_write);
             TwoPassMacroProcesser system = new TwoPassMacroProcesser();
-            system.PassOneMacroProcesser();
-            
+            String logs = system.PassOneMacroProcesser();
+            if(!logs.equals("Pass one ok")){
+                System.out.println(logs);
+            }
             buff_write.close();
             buffer_read.close();
 
